51ead973b3960f1c747dabeb118022c923b239c6
sanjugowda216 <143921320+sanjugowda216@users.noreply.github.com>
2025-01-31 14:56:05 -0800

    Add files via upload

---
+++ alkalinity.py
@@ -0,0 +1,25 @@
+def monitor():
+  try:
+    
+    val1 = 17
+    val2 = 13
+
+    alkilines = list(range(val2, val1))
+
+    current = get_alkalinity()
+    mesg = "Alkalinity OK"
+
+    if (current < alkilines[0]):
+      mesg = "Alkalinity too low!"
+    elif (current > alkilines[5]):
+      mesg = "Alkalinity too high!"
+    
+  except:
+    print("Unexpected error") 
+    
+  return mesg
+
+# Function to simulate actual fish tank monitoring
+def get_alkalinity():
+  return 9
+
---
+++ start_monitoring.py
@@ -0,0 +1,6 @@
+# start_monitoring.py
+import tkinter as tk 
+import fishtank as tank 
+my_tank = tank.FishTank() 
+my_tank.mainloop()
+
